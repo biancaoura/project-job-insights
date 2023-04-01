416b4939389de336382da1f2784eba99cb931565
Bianca Oura <biancaoura@gmail.com>
2023-04-01 19:33:13 -0300

    test: add read_brazilian_file test

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    keys = ["title", "salary", "type"]
+    assert [jobs[0][i] for i in keys if jobs[0].get(i)]
